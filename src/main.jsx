c8ad9c6df379275c91c864fa1a0beadd20bc757e
Ehsan Madadi <ehsanmadadi11@gmail.com>
2025-05-17 17:41:48 +0330

    add "SWRConfig" to main.jsx ( SWRConfig allows you to apply group or global settings.)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { SWRConfig } from 'swr'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <SWRConfig value={{
+    refreshInterval :1000
+  }}>
+    <App />
+  </SWRConfig>
+
 )
