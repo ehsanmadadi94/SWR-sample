45a9d25daadccb2f8a4690103f0a355c3586f209
Ehsan Madadi <ehsanmadadi11@gmail.com>
2025-05-19 19:27:12 +0330

    add error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import useSWR from "swr"
 
 function App() {
-  const getData=(url)=>fetch(url).then(res=>res.json())
-  const{data}=useSWR('https://68287be16b7628c529137a63.mockapi.io/Todos',getData,{
-    revalidateIfStale :false,
-    revalidateOnFocus : false
-  })
-  console.log(data)
+
+  //normal way to do it->
+  //const getData=(url)=>fetch(url).then(res=>res.json())
+
+  //better way to do it and handle errors(I just logged it , you can use the error in other ways like show it or use it to return another component)->
+  const getData=async(url)=>{
+    const res =await fetch(url);
+    if(! res.ok){
+      const error= new Error('You have an error');
+      error.info=await res.json();
+      error.status=res.status;
+      throw error
+    }
+
+    return res.json();
+
+  }
+//with options for useSWR->
+  // const{data,error}=useSWR('https://68287be16b7628c529137a63.mockapi.io/Todos',getData,{
+  //   revalidateIfStale :false,
+  //   revalidateOnFocus : false
+  // })
+
+  //use useSWR in a normal way->
+  const{data,error}=useSWR('https://68287be16b7628c529137a63.mockapi.io/Todos',getData)
+
+  console.log(data , error?.info , error?.status)
   return (
     <>
       {
